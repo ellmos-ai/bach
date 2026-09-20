739f022a021b735793c5f230bcd9e13da5386658
lukisch <55554266+lukisch@users.noreply.github.com>
2026-09-20 14:16:42 +0200

    chore(release): sync setup version

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ def _run_packaging_setup() -> None:
 
     setuptools_setup(
         name="ellmos-bach",
-        version="3.13.0",
+        version="3.14.0",
         description="BACH root-level editable install shim",
         long_description=long_description,
         long_description_content_type="text/markdown",
